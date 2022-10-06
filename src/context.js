23a83c1da9eff6801b44faf1214983ae6a48ffbe
JC Grennan <jgrennan94@gmail.com>
2022-10-06 17:41:06 +0100

    used Effect hook to fetch photo objects

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import {createContext, useState} from "react";
+import {createContext, useState, useEffect} from "react";
 
 const Context = createContext() 
 
@@ -6,8 +6,14 @@ function ContextProvider({children}) {
 
     const [photos, setPhotos] = useState()
 
+    useEffect(() => {
+        fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
+            .then(res => res.json())
+            .then(data => setPhotos(data))
+    }, [])
+
     return (
-        <Context.Provider value={{photos, setPhotos}}>{children}</Context.Provider>
+        <Context.Provider value={{photos}}>{children}</Context.Provider>
     )
 }
 
