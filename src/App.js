d2e349d90f5a094ab927190731a84c146753fd40
JC Grennan <jgrennan94@gmail.com>
2022-10-06 15:21:12 +0100

    imported pages and implemented routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Header from "./components/Header"
+import Photos from "./pages/Photos"
+import Cart from "./pages/Cart"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div>
         <Header />
+
+        <Routes>
+            <Route path="/" element={<Photos />} />
+            <Route path="/cart" element={<Cart />}/>
+        </Routes>
     </div>
   )
 }
