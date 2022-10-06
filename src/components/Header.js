9a489fcf809dd297262086aa8bced39e83ee688d
JC Grennan <jgrennan94@gmail.com>
2022-10-06 15:23:16 +0100

    used Link to route to / when title clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
+import {Link} from "react-router-dom"
+
 function Header() {
     return (
         <header>
-            <h2>Pic Some</h2>
+            <Link to="/"><h2>Pic Some</h2></Link>
             <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
         </header>
     )
