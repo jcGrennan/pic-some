fde207884e4490eaa4841ee0e15c1dbd43c1a1e8
JC Grennan <jgrennan94@gmail.com>
2022-10-06 14:57:22 +0100

    updated css import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom"
 
-import './index.css';
+import './styles.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
