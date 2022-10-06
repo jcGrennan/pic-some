ee733a0a26ed1b76d15797a34f47f7bb17c9b6be
JC Grennan <jgrennan94@gmail.com>
2022-10-06 18:26:32 +0100

    mapped over context to make photoGrid using Image

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,7 +1,20 @@
+import {useContext} from "react"
+
+import {Context} from "../Context"
+import Image from "../components/Image"
+
+
 function Photos() {
+
+    const photosArray = useContext(Context).photos
+   
+    const photoGrid = photosArray.map(photo => (
+        <Image key={photo.id} img={photo} className={photo.id - 1} />
+    ))
+
     return (
         <main className="photos">
-            <h1>Images go here</h1>
+            {photoGrid}
         </main>
     )
 }
