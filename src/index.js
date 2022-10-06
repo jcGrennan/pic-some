04a6358a46ace24e907d8e6d1350435a146cee24
JC Grennan <jgrennan94@gmail.com>
2022-10-06 15:46:15 +0100

    imported and wrapped ContextProvider around router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import {BrowserRouter} from "react-router-dom"
 
 import './styles.css';
 import App from './App';
+import {ContextProvider} from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <ContextProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ContextProvider>
 )
