05b13a50d72cab1f698b088996e16d359cc0f8f1
JC Grennan <jgrennan94@gmail.com>
2022-10-07 17:27:25 +0100

    imported toggledFavorite. used on heartIcon

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,13 @@
-import {useState} from "react"
+import {useState, useContext} from "react"
+import {Context} from "../Context"
 
 function Image({className, img}) {
 
     const [hovered, setHovered] = useState(false)
 
-    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const {toggleFavorite} = useContext(Context)
+
+    const heartIcon = hovered && <i onClick={()=> toggleFavorite(img.id)} className="ri-heart-line favorite"></i>
     const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
 
     function handleHover() {
