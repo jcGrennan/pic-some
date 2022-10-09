0de0ab66b2a16f4055fd20367aa3171621648a9e
JC Grennan <jgrennan94@gmail.com>
2022-10-09 18:55:41 +0100

    pulled conditional rendering out of return

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,12 @@ import {Context} from "../Context"
 function Header() {
 
     const {cartItems} = useContext(Context)
+    const cartClassName = cartItems.length ? "ri-shopping-cart-fill ri-fw ri-2x" : "ri-shopping-cart-line ri-fw ri-2x"
 
     return (
         <header>
             <Link to="/"><h2>Pic Some</h2></Link>
-            <Link to="/cart">
-                <i 
-                    className={
-                        cartItems.length > 0 ? 
-                            "ri-shopping-cart-fill ri-fw ri-2x" :
-                            "ri-shopping-cart-line ri-fw ri-2x"
-                    }
-                >
-                </i>
-            </Link>
+            <Link to="/cart"><i className={cartClassName}></i></Link>
         </header>
     )
 }
