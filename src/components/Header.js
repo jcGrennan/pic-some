1ab884285c0325c41de627f6b52eee1542e14416
JC Grennan <jgrennan94@gmail.com>
2022-10-09 18:51:15 +0100

    conditionally rendered full cart if cart has item

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import {Link} from "react-router-dom"
+import {useContext} from "react"
+import {Context} from "../Context"
 
 function Header() {
+
+    const {cartItems} = useContext(Context)
+
     return (
         <header>
             <Link to="/"><h2>Pic Some</h2></Link>
-            <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+            <Link to="/cart">
+                <i 
+                    className={
+                        cartItems.length > 0 ? 
+                            "ri-shopping-cart-fill ri-fw ri-2x" :
+                            "ri-shopping-cart-line ri-fw ri-2x"
+                    }
+                >
+                </i>
+            </Link>
         </header>
     )
 }
