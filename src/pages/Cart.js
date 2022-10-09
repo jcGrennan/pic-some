d66dbfd47a717516d5ebd7124c62cb1ed43157fc
JC Grennan <jgrennan94@gmail.com>
2022-10-09 19:04:34 +0100

    used context to map cart items

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,17 @@
+import {useContext} from "react"
+import {Context} from "../Context"
+
 function Cart() {
+
+    const {cartItems} = useContext(Context)
+    const cartItemElements = cartItems.map(item => (
+        <h3 key={item.id}>{item.id}</h3>
+    ))
+
     return (
         <main className="cart-page">
             <h1>Checkout</h1>
+            {cartItemElements}
         </main>
     )
 }
