24d78e4baed131840c14ac3db116c89c3197aec1
JC Grennan <jgrennan94@gmail.com>
2022-10-10 18:19:28 +0100

    tidied total variable

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,11 +5,11 @@ import CartItem from "../components/CartItem"
 function Cart() {
 
     const {cartItems} = useContext(Context)
+    const total = (cartItems.length * 4.99).toLocaleString("en-IE", {style:"currency", currency:"EUR"})
+    
     const cartItemElements = cartItems.map(item => (
         <CartItem key={item.id} item={item} />
     ))
-
-    const total = (cartItemElements.length * 4.99).toLocaleString("en-IE", {style:"currency", currency:"EUR"})
 
     return (
         <main className="cart-page">
