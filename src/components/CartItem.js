aec674d308b47c480784c162dfb1ae5025249a7c
JC Grennan <jgrennan94@gmail.com>
2022-10-11 16:13:32 +0100

    used state to display full trash when icon hovered

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,13 +1,19 @@
-import {useContext} from "react"
+import {useState, useContext} from "react"
 import {Context} from "../Context"
 
 function CartItem({item}) {
 
     const {removeFromCart} = useContext(Context)
+    const [iconClass, setIconClass] = useState("line")
 
     return (
         <div className="cart-item">
-            <i onClick={() => removeFromCart(item.id)} className="ri-delete-bin-line"></i>
+            <i 
+                onMouseOver={() => setIconClass("fill")} 
+                onMouseOut={() => setIconClass("line")} 
+                onClick={() => removeFromCart(item.id)} 
+                className={`ri-delete-bin-${iconClass}`}
+            ></i>
             <img src={item.url} width="130px" />
             <p>€4.99</p>
         </div>
