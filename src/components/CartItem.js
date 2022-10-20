6c1587483cc3fcd15678a43cf288952368368d74
JC Grennan <jgrennan94@gmail.com>
2022-10-20 17:22:16 +0100

    refactored to use custom hover hook

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,19 +1,21 @@
 import {useState, useContext} from "react"
 import {Context} from "../Context"
 import PropTypes from "prop-types"
+import useHover from "../hooks/useHover"
 
 function CartItem({item}) {
 
     const {removeFromCart} = useContext(Context)
-    const [iconClass, setIconClass] = useState("line")
+    const [hovered, mouseRef] = useHover()
+
+    const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
 
     return (
         <div className="cart-item">
             <i 
-                onMouseOver={() => setIconClass("fill")} 
-                onMouseOut={() => setIconClass("line")} 
                 onClick={() => removeFromCart(item.id)} 
-                className={`ri-delete-bin-${iconClass}`}
+                className={iconClassName}
+                ref={mouseRef}
             ></i>
             <img alt="cart item" src={item.url} width="130px" />
             <p>€4.99</p>
