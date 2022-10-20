ec852860dc1713b80580bb01549e564b481f409b
JC Grennan <jgrennan94@gmail.com>
2022-10-20 17:05:43 +0100

    set up useEffect to add event listeners to ref.

--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -1,16 +1,22 @@
 import {useState, useEffect, useRef} from "react"
 
 function useHover() {
-    const mouseRef = useRef(null)
     const [hovered, setHovered] = useState(false)
+    const mouseRef = useRef(null)
 
+    
     function enter() {
         setHovered(true)
     }
-
+    
     function leave() {
         setHovered(false)
     }
+
+    useEffect(() => {
+        mouseRef.current.addEventListener("mouseenter", enter)
+        mouseRef.current.addEventListener("mouseleave", leave)
+    },[])
 }
 
 export default Hover
