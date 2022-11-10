e49ab4c1631f4e669821a4f0c3db8ff72aa99336
JC Grennan <jgrennan94@gmail.com>
2022-11-10 16:24:26 +0000

    comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+// importing ReactDOM for rendering, and BrowserRouter to Route pages from App Routes.
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom"
 
+// importing stylesheet, App, and the Context Provider in order to pass state only to where its needed.
 import './styles.css';
 import App from './App';
 import {ContextProvider} from './Context';
 
+/* rendering the App wrapped in the Context Provider so it can pass data anywhere in the app,
+    and the Browser Router so the user can route between pages.*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ContextProvider>
