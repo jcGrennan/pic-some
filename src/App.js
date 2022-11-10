45d9358aeadaee06b674ff05af097c7d1ac9c3aa
JC Grennan <jgrennan94@gmail.com>
2022-11-10 16:14:53 +0000

    comments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+// importing pages, components, and React Router components to route pages.
 import Header from "./components/Header"
 import Photos from "./pages/Photos"
 import Cart from "./pages/Cart"
 import {Routes, Route} from "react-router-dom"
 
+// composing jsx for App and routing paths
 function App() {
   return (
     <div>
@@ -16,4 +18,5 @@ function App() {
   )
 }
 
+// exporting App to be imported in index.js
 export default App
