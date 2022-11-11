60c954d8e4a261358f284663e36d804b2fdfd6c8
JC Grennan <jgrennan94@gmail.com>
2022-11-11 18:08:55 +0000

    comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
+// importing Link component for routing between pages, context hook, and my context
 import {Link} from "react-router-dom"
 import {useContext} from "react"
 import {Context} from "../Context"
 
+
+// composing Header component
 function Header() {
 
+    // pulling cart items from Context using context hook
     const {cartItems} = useContext(Context)
+
+    // using cartItems length and a ternary to determine whether the cart icon is filled or not
     const cartClassName = cartItems.length ? "ri-shopping-cart-fill ri-fw ri-2x" : "ri-shopping-cart-line ri-fw ri-2x"
 
+    // composing jsx for Header component
     return (
         <header>
             <Link to="/"><h2>Pic Some</h2></Link>
@@ -15,4 +22,5 @@ function Header() {
     )
 }
 
+// exporting Header to be rendered in App
 export default Header
