9dfb2325c637d2d0389e02a326e25f2665392f83
JC Grennan <jgrennan94@gmail.com>
2022-11-11 16:18:02 +0000

    comment

--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -1,7 +1,7 @@
 /* A custom hook to handle hovering. Not necessary but shows 
 possibility of custom hook use for potentially larger websites*/
 
-// importing react hooks. Ref will be used to determine what is being hovered on
+// importing react hooks. Ref will be used to determine what elements get event listeners
 import {useState, useEffect, useRef} from "react"
 
 //declaring my custom hook component
@@ -22,7 +22,8 @@ function useHover() {
     }
 
     /* setting up an effect that will add and remove event listeners 
-    to whatever is being hovered over that will use the custom hook*/
+    to whatever elements I give the mouseRef. Has to be cleaned up as
+    the components the elements are within will not always be mounted.*/
     useEffect(() => {
         let domNodeRef = mouseRef.current
         domNodeRef.addEventListener("mouseenter", enter)
